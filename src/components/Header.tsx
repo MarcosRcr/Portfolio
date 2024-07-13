efe5d0e728a7e865e84c9ffdc418b39713f2bc84
Marcosrcr <marcosr.cr19@gmail.com>
2024-07-13 15:17:04 -0600

    chore: Update Firebase configuration and dependencies

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,3 @@
-import { MdOutlineSimCardDownload } from "react-icons/md";
 
 export const Header = () => {
   return (
@@ -8,7 +7,7 @@ export const Header = () => {
         <p className="text-lg italic">Hi, I'm</p>
         <h1 className="text-4xl font-semibold">Marcos Cuellar</h1>
         <p className="w-96">I am a computer engineer. I currently live in CDMX, Mexico, and I am a web developer with skills in database management, web design, data analysis, and Python programming. My favorite technologies are TypeScript, React, Nest, PostgreSQL, and Python. Ready to network? 🫂</p>
-        <a href="/cv-marcos_cuellar-en.pdf" download className="flex items-center justify-center gap-5 mt-5 py-2 px-1 w-40 border-2 border-black rounded-xl hover:bg-slate-100 transition-all ">Download CV <MdOutlineSimCardDownload/></a>
+        
       </div>
     </header>
   );
